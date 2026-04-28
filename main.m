47f36e6fe3e00c7bef1a567863d8b3fe75ed9164
ghs-123-code <2843916992@qq.com>
2026-04-28 20:28:52 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,8 +1,7 @@
 clear
 
 addpath('load_H_matrix');
-addpath('5GNR');
-addpath('5GNR\base_matrices');
+addpath(genpath('5GNR'));
 ZC=384;
 bg = Circular_shift_matrix(1,ZC); % 1 for BG1, 2 for BG2
 bg = bg(1:13,1:35);  % code rate 2/3
@@ -22,7 +21,7 @@ max_err = 100;
 %Supported constellation types:
 % QPSK, 16QAM, 256QAM,  All in Gray Lableing.
 %Naive Modulation, e.g., x1,x2,...,xm are mapped into one symbol and then transmitted
-%BICM-style demodulation, i.e., independent bit level, one received symbol ¡û¡ú m LLRs
+%BICM-style demodulation, i.e., independent bit level, one received symbol Â¡Ã»Â¡Ãº m LLRs
 [num_block_err, num_bit_err, num_iter, num_runs] = simulation(esno_vec, conste_name, P, H_row_one_absolute_index, H_comlumn_one_relative_index, N, M, K, vn_degree, cn_degree, ...
     vn_distribution, cn_distribution, max_iter, max_runs, resolution, max_err, puncture, ZC);
 disp('BLER simulation is finished.')
