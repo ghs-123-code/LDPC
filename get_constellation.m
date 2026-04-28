239d077dd510cd9186b2029c3fd0ed7de3476e1e
ghs-123-code <2843916992@qq.com>
2026-04-28 20:15:09 +0800

    get_constellation.m

--- get_constellation.m
+++ get_constellation.m
@@ -1,17 +1,7 @@
-function [constellation, rho_inv, base_vec, demod_indices, is2D, m, M] = get_constellation(name, N)
+function [constellation, rho_inv, base_vec, demod_indices, demod_indices0, is2D, m, M] = get_constellation(name, N)
 %This is a very carefully designed High-order Modultion MATLAB program.
 
-%You may see many 'strange' operations below. Such operations are needed to obtain a
-%very fast MATLAB codes that has similar speed to C language.
 switch name
-    case 'BPSK'
-        constellation = [];
-        demod_indices = [];
-        base_vec = [];
-        rho_inv = [];%Avoid using 'if' or 'switch' to find the target constellation point. We use 'rho_inv' to directly locate the target constellation point.
-        is2D = 0;% 2 dimensions ¡ú 1; otherwise 0.
-        m = 1;%how many bits per symbol.
-        M = 2^m;
     case 'QPSK'
         constellation = [-1/sqrt(2) + 1j * 1/sqrt(2); -1/sqrt(2) - 1j * 1/sqrt(2); 1/sqrt(2) + 1j * 1/sqrt(2); 1/sqrt(2) - 1j * 1/sqrt(2);];
         bit_label = [0 1; 1 1; 0 0; 1 0];
@@ -25,12 +15,17 @@ switch name
         base_vec(2 : m : N) = 1;
         M = 2^m;
         demod_indices = zeros(M/2, m);
+        demod_indices0 = zeros(M/2, m);
         for i_m = 1 : m
             cnt_1 = 1;
+            cnt_0 = 1;
             for k = 1 : M
                 if bit_label(k, i_m)
                     demod_indices(cnt_1, i_m) = k;
                     cnt_1 = cnt_1 + 1;
+                else
+                    demod_indices0(cnt_0, i_m) = k;
+                    cnt_0 = cnt_0 + 1;
                 end
             end
         end
@@ -53,187 +48,23 @@ switch name
         base_vec(4 : m : N) = 1;
         M = 2^m;
         demod_indices = zeros(M/2, m);
+        demod_indices0 = zeros(M/2, m);
         for i_m = 1 : m
             cnt_1 = 1;
+            cnt_0 = 1;
             for k = 1 : M
                 if bit_label(k, i_m)
                     demod_indices(cnt_1, i_m) = k;
                     cnt_1 = cnt_1 + 1;
+                else
+                     demod_indices0(cnt_0, i_m) = k;
+                    cnt_0 = cnt_0 + 1;
                 end
             end
         end
         demod_indices = demod_indices(:);
-    case '64QAM'
-        constellation = zeros(64, 1);
-        cnt = 1;
-        for x_axis = -7 : 2 : 7
-            for y_axis = 7 : -2  : -7
-                constellation(cnt) = x_axis + 1j * y_axis;
-                cnt = cnt + 1;
-            end
-        end
-        constellation = constellation(:)/sqrt(42);
-        bit_label = ...
-            [1 0 1 1 1 1;
-            1 0 1 1 1 0;
-            1 0 1 0 1 0;
-            1 0 1 0 1 1;
-            1 1 1 0 1 1;
-            1 1 1 0 1 0;
-            1 1 1 1 1 0;
-            1 1 1 1 1 1;
-            1 0 1 1 0 1;
-            1 0 1 1 0 0;
-            1 0 1 0 0 0;
-            1 0 1 0 0 1;
-            1 1 1 0 0 1;
-            1 1 1 0 0 0;
-            1 1 1 1 0 0;
-            1 1 1 1 0 1;
-            1 0 0 1 0 1;
-            1 0 0 1 0 0;
-            1 0 0 0 0 0;
-            1 0 0 0 0 1;
-            1 1 0 0 0 1;
-            1 1 0 0 0 0;
-            1 1 0 1 0 0;
-            1 1 0 1 0 1;
-            1 0 0 1 1 1;
-            1 0 0 1 1 0;
-            1 0 0 0 1 0;
-            1 0 0 0 1 1;
-            1 1 0 0 1 1;
-            1 1 0 0 1 0;
-            1 1 0 1 1 0;
-            1 1 0 1 1 1;
-            0 0 0 1 1 1;
-            0 0 0 1 1 0;
-            0 0 0 0 1 0;
-            0 0 0 0 1 1;
-            0 1 0 0 1 1;
-            0 1 0 0 1 0;
-            0 1 0 1 1 0;
-            0 1 0 1 1 1;
-            0 0 0 1 0 1;
-            0 0 0 1 0 0;
-            0 0 0 0 0 0;
-            0 0 0 0 0 1;
-            0 1 0 0 0 1;
-            0 1 0 0 0 0;
-            0 1 0 1 0 0;
-            0 1 0 1 0 1;
-            0 0 1 1 0 1;
-            0 0 1 1 0 0;
-            0 0 1 0 0 0;
-            0 0 1 0 0 1;
-            0 1 1 0 0 1;
-            0 1 1 0 0 0;
-            0 1 1 1 0 0;
-            0 1 1 1 0 1;
-            0 0 1 1 1 1;
-            0 0 1 1 1 0;
-            0 0 1 0 1 0;
-            0 0 1 0 1 1;
-            0 1 1 0 1 1;
-            0 1 1 0 1 0;
-            0 1 1 1 1 0;
-            0 1 1 1 1 1;];
-        base = [32; 16; 8; 4; 2; 1];
-        rho = bit_label * base + 1;
-        rho_inv(rho) = 1 : 64;
-        is2D = 1;
-        m = 6;
-        base_vec = zeros(N, 1);
-        base_vec(1 : m : N) = 32;
-        base_vec(2 : m : N) = 16;
-        base_vec(3 : m : N) = 8;
-        base_vec(4 : m : N) = 4;
-        base_vec(5 : m : N) = 2;
-        base_vec(6 : m : N) = 1;
-        M = 2^m;
-        demod_indices = zeros(M/2, m);
-        for i_m = 1 : m
-            cnt_1 = 1;
-            for k = 1 : M
-                if bit_label(k, i_m)
-                    demod_indices(cnt_1, i_m) = k;
-                    cnt_1 = cnt_1 + 1;
-                end
-            end
-        end
-        demod_indices = demod_indices(:);
-    case '4ASK'
-        constellation = (-3 : 2 : 3)'/sqrt(5);
-        bit_label = [0 0; 0 1; 1 1; 1 0];
-        base = [2; 1];
-        rho = bit_label * base + 1;
-        rho_inv(rho) = 1 : 4;
-        is2D = 0;
-        m = 2;
-        base_vec = zeros(N, 1);
-        base_vec(1 : m : N) = 2;
-        base_vec(2 : m : N) = 1;
-        M = 2^m;
-        demod_indices = zeros(M/2, m);
-        for i_m = 1 : m
-            cnt_1 = 1;
-            for k = 1 : M
-                if bit_label(k, i_m)
-                    demod_indices(cnt_1, i_m) = k;
-                    cnt_1 = cnt_1 + 1;
-                end
-            end
-        end
-        demod_indices = demod_indices(:);
-    case '8ASK'
-        constellation = (-7 : 2 : 7)'/sqrt(21);
-        bit_label = [0 0 0; 0 0 1; 0 1 1; 0 1 0; 1 1 0; 1 1 1; 1 0 1; 1 0 0];
-        base = [4; 2; 1];
-        rho = bit_label * base + 1;
-        rho_inv(rho) = 1 : 8;
-        is2D = 0;
-        m = 3;
-        base_vec = zeros(N, 1);
-        base_vec(1 : m : N) = 4;
-        base_vec(2 : m : N) = 2;
-        base_vec(3 : m : N) = 1;
-        M = 2^m;
-        demod_indices = zeros(M/2, m);
-        for i_m = 1 : m
-            cnt_1 = 1;
-            for k = 1 : M
-                if bit_label(k, i_m)
-                    demod_indices(cnt_1, i_m) = k;
-                    cnt_1 = cnt_1 + 1;
-                end
-            end
-        end
-        demod_indices = demod_indices(:);
-    case '16ASK'
-        constellation = (-15 : 2 : 15)'/sqrt(85);
-        bit_label = [0 0 0 0; 0 0 0 1; 0 0 1 1; 0 0 1 0; 0 1 1 0; 0 1 1 1; 0 1 0 1; 0 1 0 0; 1 1 0 0; 1 1 0 1; 1 1 1 1; 1 1 1 0; 1 0 1 0; 1 0 1 1; 1 0 0 1; 1 0 0 0];
-        base = [8; 4; 2; 1];
-        rho = bit_label * base + 1;
-        rho_inv(rho) = 1 : 16;
-        is2D = 0;
-        m = 4;
-        base_vec = zeros(N, 1);
-        base_vec(1 : m : N) = 8;
-        base_vec(2 : m : N) = 4;
-        base_vec(3 : m : N) = 2;
-        base_vec(4 : m : N) = 1;
-        M = 2^m;
-        demod_indices = zeros(M/2, m);
-        for i_m = 1 : m
-            cnt_1 = 1;
-            for k = 1 : M
-                if bit_label(k, i_m)
-                    demod_indices(cnt_1, i_m) = k;
-                    cnt_1 = cnt_1 + 1;
-                end
-            end
-        end
-        demod_indices = demod_indices(:);
+    
+    
     case '256QAM'
         m = 8;
         M = 2^m;
@@ -269,12 +100,17 @@ switch name
         base_vec(7 : m : N) = 2;
         base_vec(8 : m : N) = 1;
         demod_indices = zeros(M/2, m);
+        demod_indices0 = zeros(M/2, m);
         for i_m = 1 : m
             cnt_1 = 1;
+            cnt_0 = 1;
             for k = 1 : M
                 if bit_label(k, i_m)
                     demod_indices(cnt_1, i_m) = k;
                     cnt_1 = cnt_1 + 1;
+                else
+                    demod_indices0(cnt_0, i_m) = k;
+                    cnt_0 = cnt_0 + 1;
                 end
             end
         end
